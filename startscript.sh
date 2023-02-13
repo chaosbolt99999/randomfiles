5979b93d9ffff256472605da48c9c855ae76097f
chaosbolt99999 <89195237+chaosbolt99999@users.noreply.github.com>
2023-02-13 19:45:08 +0800

    Update startscript.sh

--- startscript.sh
+++ startscript.sh
@@ -3,7 +3,7 @@
 /usr/local/bin/entry_point.sh
 
 tmux new-session -d
-tmux send-keys '/data/scripts/restart.sh' C-m
+tmux send-keys '/root/scripts/restart.sh' C-m
 tmux rename-window 'minecraft'
 
 /usr/sbin/sshd -D -e
